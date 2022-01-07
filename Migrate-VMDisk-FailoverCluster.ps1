96c786bf95c24af2cf3ac5b5f23bc7ca3f8cbae8
Simon Babič <simon.babic@actual-it.si>
2022-01-07 09:30:24 +0100

    Failover cluster move storage commit

---
+++ Migrate-VMDisk-FailoverCluster.ps1
@@ -0,0 +1,26 @@
+﻿#Simple script to migrate storage of a virtual machine properly and delete files from old storage.
+#
+Write-Host "##### Start script #####" -ForegroundColor Green
+Write-Host
+$hyhost = Read-Host -Prompt 'Input Hyper-V host where VM is running'
+Write-Host
+$vm = Read-Host -Prompt 'Input VM name that you wnt to migrate storage'
+Write-Host
+$path = Read-Host -Prompt 'Input full path where do you want to migrate VM - Write VM Configuration folder at the end'
+Write-Host
+$M = Get-VM $vm
+$path_old = $M.ConfigurationLocation
+
+
+#Move storage command
+
+Write-host "##### Moving storage #####" -ForegroundColor Green
+Write-host
+Move-VMStorage -ComputerName $hyhost $vm -DestinationStoragePath $path
+
+#Delete folder from old storage
+
+Remove-Item $path_old -Force  -Recurse -ErrorAction SilentlyContinue
+Write-host "##### Configuration files are removed from old location #####"
+Write-Host
+Write-Host "your VM has been moved from " $path_old " to: " $path -ForegroundColor Green
